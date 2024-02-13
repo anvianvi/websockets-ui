2b386aef39a73efebc75bebd127196242abf964d
Pavel <verbipo@gmail.com>
2024-02-13 12:51:53 +0100

    feat: init websocket server

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import { httpServer } from "./src/http_server/index.js";
+import { runWebSocketServer } from "./src/websocket_server/index.js";
 
-const HTTP_PORT = 8181;
+const HTTP_PORT = Number(process.env.HTTP_PORT ?? 8181);
+const WEBSOCKET_PORT = Number(process.env.WEBSOCKET_PORT ?? 3000);
 
-console.log(`Start static http server on the ${HTTP_PORT} port!`);
 httpServer.listen(HTTP_PORT);
+runWebSocketServer(WEBSOCKET_PORT);
+
+console.log(`Static HTTP server is running on port ${HTTP_PORT}!`);
+console.log(`WebSocket server is running on port ${WEBSOCKET_PORT}!`);
+
