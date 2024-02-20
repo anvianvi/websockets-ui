e954f6cac0c490fd9aeab167217f493f068c9df7
Pavel <verbipo@gmail.com>
2024-02-20 13:03:09 +0100

    refactor: regestrationhandler

--- src/websocket_server/heandlers/registration.ts
+++ src/websocket_server/heandlers/registration.ts
@@ -1,34 +1,31 @@
 import { Player, players } from "../../db";
 
 export function handleRegistration(input: string) {
-  let data = JSON.parse(input);
-  console.log(players.some((player) => player.name === data.name));
+  const data = JSON.parse(input);
+  const existingPlayer = players.find((player) => player.name === data.name);
 
-  if (players.some((player) => player.name === data.name)) {
-    const response = {
+  if (existingPlayer) {
+    return JSON.stringify({
       name: data.name,
       index: null,
       error: true,
       errorText: "Player already exists",
-    };
-    return JSON.stringify(response);
-  } else {
-    // Create a new player
-    const id = players.length;
-    const player: Player = {
-      index: id,
-      name: data.name,
-      password: data.password,
-    };
-
-    players.push(player);
-
-    const response = {
-      name: data.name,
-      index: id,
-      error: false,
-      errorText: "",
-    };
-    return JSON.stringify(response);
+    });
   }
+
+  const id = players.length;
+  const player: Player = {
+    index: id,
+    name: data.name,
+    password: data.password,
+  };
+
+  players.push(player);
+
+  return JSON.stringify({
+    name: data.name,
+    index: id,
+    error: false,
+    errorText: "",
+  });
 }
