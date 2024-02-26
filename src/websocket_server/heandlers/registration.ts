b097d1f4e654702b3b25e0199cab9e404e55c7df
Pavel <verbipo@gmail.com>
2024-02-26 21:12:02 +0100

    feat:  +5 User validation

--- src/websocket_server/heandlers/registration.ts
+++ src/websocket_server/heandlers/registration.ts
@@ -5,12 +5,21 @@ export function handleRegistration(input: string) {
   const existingPlayer = players.find((player) => player.name === data.name);
 
   if (existingPlayer) {
-    return JSON.stringify({
-      name: data.name,
-      index: existingPlayer.index,
-      error: false,
-      errorText: "You are loggedIn",
-    });
+    if (existingPlayer.password === data.password) {
+      return JSON.stringify({
+        name: data.name,
+        index: existingPlayer.index,
+        error: false,
+        errorText: "You are loggedIn",
+      });
+    } else {
+      return JSON.stringify({
+        name: data.name,
+        password: data.password,
+        error: true,
+        errorText: "Incorrect password",
+      });
+    }
   }
 
   const id = players.length;
